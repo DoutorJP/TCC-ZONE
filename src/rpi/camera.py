97226ff5182e293891b78d2c466be35336a5218d
João Pedro de Moura <72107536+DoutorJP@users.noreply.github.com>
2024-08-27 08:58:25 -0300

    CAMERA FUNFA

--- src/rpi/camera.py
+++ src/rpi/camera.py
@@ -1,4 +1,6 @@
 import cv2
+import imutils
+import numpy as np
 webcam = cv2.VideoCapture(0)
 
 if webcam.isOpened():
@@ -9,6 +11,32 @@ if webcam.isOpened():
         key = cv2.waitKey(5)
         if key==27:
             break
-    cv2.imwrite("img.PNG", frame)
+    frame = cv2.resize(frame, (620,480) )
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    gray = cv2.bilateralFilter(gray, 13, 15, 15)
+    edged = cv2.Canny(gray, 30, 200) 
+    contours=cv2.findContours(edged.copy(),cv2.RETR_TREE,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    contours = sorted(contours,key=cv2.contourArea, reverse = True)[:10]
+    screenCnt = None
+    for c in contours:
+
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    
+    mask = np.zeros(gray.shape,np.uint8)
+    new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+    new_image = cv2.bitwise_and(frame,frame,mask=mask)
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+    cv2.imwrite("img.PNG", Cropped)
+
 webcam.release()
 cv2.destroyAllWindows()
--- src/rpi/firebase.py
+++ src/rpi/firebase.py
@@ -16,9 +16,11 @@ c = Carro.Carro(placa, modelo, dono)
 
 dados = {'Placa': c.placa, 'Modelo': c.modelo, 'Dono': c.dono, 'DataEntrada': c.dataentrada()}
 requesicao = requests.post(f'{link}/Carros/.json', data=json.dumps(dados))
-'''
+
 
 # ------------------------- LEITURA (GET)--------------------------
+'''
 requisicao = requests.get(f'{link}/.json')
 dic_requisicao = requisicao.json()
-print(dic_requisicao['Carros'])
+#print(dic_requisicao['Carros'])
+print(dic_requisicao)
