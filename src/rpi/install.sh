13fbe64b7c6022a2fdc2002cccd22de089e9c68e
DoutorJP <jpmv14@gmail.com>
2024-09-18 16:58:31 -0400

    adicionadas dependencias e script de instalação

---
+++ src/rpi/install.sh
@@ -0,0 +1,5 @@
+# Instala o tesseract-ocr e libtesseract-dev pelo apt
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
+# Instala as dependencias do pip
+pip install -r requirements.txt
