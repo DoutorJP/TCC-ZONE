967b5476058dd66113f50e5452c2b8d84c8c8035
ZilchHarpy <102772898+ZilchHarpy@users.noreply.github.com>
2024-09-19 03:12:37 -0300

    Add files via upload

---
+++ src/rpi/util.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+def adjust_brightness(image, alpha, beta):
+    adjusted = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return adjusted
+
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+        for i in np.arange(0, 256)]).astype("uint8")
+
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
