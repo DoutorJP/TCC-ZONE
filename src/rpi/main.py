8383ab258cef466b8f75147d90f16927c4ad7c70
DoutorJP <jpmv14@gmail.com>
2024-09-28 08:05:55 -0400

    atualizada a estrutura do projeto, nomes de arquivos, e o modulo crud.py (firebase)

--- src/rpi/main.py
+++ src/rpi/main.py
@@ -1,13 +1,14 @@
-from util import *
-
+from utils import ocr_utils
+from utils import crud
 
 def main():
     source = "Car_Images/car_1.jpg"
-
+    
     #find_Roi_Plate(source)
     #Pre_Processing_Roi()
     #OCR_Plate()
-
+    
+    
 
 if __name__ == "__main__":
     main()
---
+++ src/rpi/utils/crud.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import Carro
+from datetime import datetime
+import leitura
+
+link = "https://dbteste-449d5-default-rtdb.firebaseio.com"
+
+
+def escrever_dados(carro):
+    dados = {'Placa': carro.placa, 'Modelo': carro.modelo, 'Dono': carro.dono, 'DataEntrada': carro.dataentrada()}
+    requesicao = requests.post(f'{link}/Carros/.json', data=json.dumps(dados))
+
+
+def ler_dados():
+    requisicao = requests.get(f'{link}/.json')
+    dic_requisicao = requisicao.json()
+
+    #print(dic_requisicao['Carros'])
+    #print(dic_requisicao)
